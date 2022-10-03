be20d264d5568141139ae90ae3079229b7ecd2c5
olenacernova <elena.c@find-bride.com>
2022-10-03 10:52:10 +0300

    23

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,16 +4,19 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
     return (
-        <div>
-        My posts
-        <div>
-            New post
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <div>
+                <textarea></textarea>
+            </div>
+            <div>
+                <button>Add button</button>
+            </div>
+            <div className={s.posts}>
+                <Post message={`Hi, how are you?`} likesCount={15}/>
+                <Post message={`It's my first message`} likesCount={20}/>
+            </div>
         </div>
-        <div className={s.posts}>
-            <Post message={`Hi, how are you?`} likesCount={15} />
-            <Post message={`It's my first message`} likesCount={20} />
-        </div>
-    </div>
     );
 }
 
