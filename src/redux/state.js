29b10a2fb223841e6e5a4b15d56f0708353128a4
olenacernova <elena.c@find-bride.com>
2022-12-11 16:07:23 +0200

    37

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,66 +1,68 @@
-let rerenderEntireTree = () => {}
+let store = {
+    _state : {
+        profilePage: {
+            posts: [
+                {
+                    id: 1,
+                    message: "Hi!",
+                    likesCount: 15,
+                    src: "https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-8-avatar-2754583_120515.png"
+                },
+                {
+                    id: 2,
+                    message: "How are you?",
+                    likesCount: 20,
+                    src: "https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-8-avatar-2754583_120515.png"
+                },
+            ],
+            newPostText: 'it-kamasutra.com',
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: "Dimych"},
+                {id: 2, name: "Andrey"},
+                {id: 3, name: "Sveta"},
+                {id: 4, name: "Sasha"},
+                {id: 5, name: "Victor"},
+                {id: 6, name: "Valera"},
+            ],
 
-let state = {
-    profilePage: {
-        posts: [
-            {
-                id: 1,
-                message: "Hi!",
-                likesCount: 15,
-                src: "https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-8-avatar-2754583_120515.png"
-            },
-            {
-                id: 2,
-                message: "How are you?",
-                likesCount: 20,
-                src: "https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-8-avatar-2754583_120515.png"
-            },
-        ],
-        newPostText: 'it-kamasutra.com',
+            messages: [
+                {id: 1, message: "Hi!"},
+                {id: 2, message: "How is your it-kamasutra?"},
+                {id: 3, message: "Yo"},
+                {id: 4, message: "Yo"},
+                {id: 5, message: "Yo"},
+            ],
+        },
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: "Dimych"},
-            {id: 2, name: "Andrey"},
-            {id: 3, name: "Sveta"},
-            {id: 4, name: "Sasha"},
-            {id: 5, name: "Victor"},
-            {id: 6, name: "Valera"},
-        ],
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {},
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0,
+            src: "https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-8-avatar-2754583_120515.png",
+        };
 
-        messages: [
-            {id: 1, message: "Hi!"},
-            {id: 2, message: "How is your it-kamasutra?"},
-            {id: 3, message: "Yo"},
-            {id: 4, message: "Yo"},
-            {id: 5, message: "Yo"},
-        ],
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     },
 }
 
 // for debug
 // window.state = state;
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0,
-        src: "https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-8-avatar-2754583_120515.png",
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
+window.store = store;
