b5c0337532201235a995c215b91b81748a66375f
olenacernova <elena.c@find-bride.com>
2023-06-14 17:15:04 +0300

    75 redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+// import {Form, Field} from "react-final-form";
+import {Field, reduxForm} from "redux-form";
+
+
+export const LoginForm = (props) => {
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <form onSubmit={props.handleSubmit}>
+                <div><Field placeholder={"Login"} name={"login"} component={"input"}/></div>
+                <div><Field placeholder={"Password"} name={"password"} component={"input"}/></div>
+                <div><label><Field  component={"input"} name={"rememberMe"} type="checkbox" />remember me</label></div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+
+        </div>
+    );
+}
+
+export const LoginReduxForm = reduxForm({form: "login"})(LoginForm)
 
 export const Login = () => {
-    return <h1>Login</h1>;
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
+    );
 }
