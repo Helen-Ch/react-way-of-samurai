0f33c2f7dbacbe9647c516bbaced8e3bc49c1d5b
olenacernova <elena.c@find-bride.com>
2023-07-09 20:56:33 +0300

    92 react-test-renderer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import store from "./redux/redux-store";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -10,7 +11,7 @@ import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
-import {connect} from "react-redux";
+import {connect, Provider} from "react-redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 
@@ -18,33 +19,32 @@ class App extends React.Component {
     componentDidMount() {
         this.props.initializeApp();
     }
+
     render() {
         if (!this.props.initialized) {
             return <Preloader/>
         } else {
             return (
-                <BrowserRouter>
-                    <div className='app-wrapper'>
-                        <HeaderContainer/>
-                        <Navbar/>
-                        <div className='app-wrapper-content'>
-                            <Routes>
-                                <Route path='/dialogs/*' element={<DialogsContainer/>}/>
-                                <Route path='/profile'>
-                                    <Route index element={<ProfileContainer/>}/>
-                                    <Route path=":userId" element={<ProfileContainer/>}/>
-                                </Route>
-                                <Route path='/users'
-                                       element={<UsersContainer/>}/>
-                                <Route path='/news' element={<News/>}/>
-                                <Route path='/news' element={<News/>}/>
-                                <Route path='/music' element={<Music/>}/>
-                                <Route path='/settings' element={<Settings/>}/>
-                                <Route path='/login' element={<Login/>}/>
-                            </Routes>
-                        </div>
+                <div className='app-wrapper'>
+                    <HeaderContainer/>
+                    <Navbar/>
+                    <div className='app-wrapper-content'>
+                        <Routes>
+                            <Route path='/dialogs/*' element={<DialogsContainer/>}/>
+                            <Route path='/profile'>
+                                <Route index element={<ProfileContainer/>}/>
+                                <Route path=":userId" element={<ProfileContainer/>}/>
+                            </Route>
+                            <Route path='/users'
+                                   element={<UsersContainer/>}/>
+                            <Route path='/news' element={<News/>}/>
+                            <Route path='/news' element={<News/>}/>
+                            <Route path='/music' element={<Music/>}/>
+                            <Route path='/settings' element={<Settings/>}/>
+                            <Route path='/login' element={<Login/>}/>
+                        </Routes>
                     </div>
-                </BrowserRouter>
+                </div>
             );
         }
     }
@@ -54,4 +54,12 @@ const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 })
 
-export default connect(mapStateToProps, {initializeApp})(App);
+let AppContainer = connect(mapStateToProps, {initializeApp})(App);
+
+export const SamuraiJSApp = (props) => {
+    return <BrowserRouter>
+        <Provider store={store}>
+            <AppContainer/>
+        </Provider>
+    </BrowserRouter>
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import * as serviceWorker from './serviceWorker';
 
-import store from "./redux/redux-store";
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import {Provider} from "react-redux";
+import {SamuraiJSApp} from './App';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+    <SamuraiJSApp/>,
     document.getElementById('root')
 );
 
