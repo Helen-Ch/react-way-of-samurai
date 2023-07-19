348c8f7ef2871208e967c8fd15cfce33f1d14b08
olenacernova <elena.c@find-bride.com>
2023-07-19 17:26:00 +0300

    Updates

---
+++ precache-manifest.b03721349fe4aaaf4ec8eb47e2903f46.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd91c59d472312c94bcca1219b1506a",
+    "url": "/react-way-of-samurai/index.html"
+  },
+  {
+    "revision": "16ca13895b8485d6ecc2",
+    "url": "/react-way-of-samurai/static/css/3.bd2e3ea0.chunk.css"
+  },
+  {
+    "revision": "e5919ac14f042e405169",
+    "url": "/react-way-of-samurai/static/css/4.2f3abd74.chunk.css"
+  },
+  {
+    "revision": "f90f1c134059167c7a4b",
+    "url": "/react-way-of-samurai/static/css/main.a2dac1f8.chunk.css"
+  },
+  {
+    "revision": "991f4152cb7ece75a8b0",
+    "url": "/react-way-of-samurai/static/js/2.78d81f16.chunk.js"
+  },
+  {
+    "revision": "48a1c4ae8665d4eb1a4f7ffa1018d251",
+    "url": "/react-way-of-samurai/static/js/2.78d81f16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ca13895b8485d6ecc2",
+    "url": "/react-way-of-samurai/static/js/3.4aa7eca2.chunk.js"
+  },
+  {
+    "revision": "e5919ac14f042e405169",
+    "url": "/react-way-of-samurai/static/js/4.64a599fb.chunk.js"
+  },
+  {
+    "revision": "f90f1c134059167c7a4b",
+    "url": "/react-way-of-samurai/static/js/main.92d894f6.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-way-of-samurai/static/js/main.92d894f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607b4471aef604c01d8e",
+    "url": "/react-way-of-samurai/static/js/runtime-main.f223fa97.js"
+  },
+  {
+    "revision": "89fe4bcba4aa7c22aae0c022f48c6cc1",
+    "url": "/react-way-of-samurai/static/media/loader.89fe4bcb.svg"
+  },
+  {
+    "revision": "7861dadc7807c489531e672f28d0ecec",
+    "url": "/react-way-of-samurai/static/media/user.7861dadc.png"
+  }
+]);
