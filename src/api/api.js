06ea1638973b6198e4ae04e57565fa026ff3bde0
olenacernova <elena.c@find-bride.com>
2023-07-24 13:21:28 +0300

    98 captcha

--- src/api/api.js
+++ src/api/api.js
@@ -12,8 +12,8 @@ export const authAPI = {
     me() {
         return instance.get(`auth/me`);
     },
-    login(email, password, rememberMe = false) {
-        return instance.post(`auth/login`, {email, password, rememberMe});
+    login(email, password, rememberMe = false, captcha = null) {
+        return instance.post(`auth/login`, {email, password, rememberMe, captcha});
     },
     logout() {
         return instance.delete(`auth/login`);
@@ -64,6 +64,12 @@ export const profileAPI = {
 
     saveProfile(profile) {
         return instance.put(`profile`, profile);
+    }
+};
+
+export  const securityAPI = {
+    getCaptchaUrl() {
+        return instance.get(`security/get-captcha-url`);
     }
 };
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,13 +8,17 @@ import {login} from "../../redux/auth-reducer";
 import {Navigate} from "react-router-dom";
 
 
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 {createField("Email", "email", [required], Input)}
                 {createField("Password", "password", [required], Input, {type: "password"})}
                 {createField(null, "rememberMe", [], Input, {type: "checkbox"}, "remember me")}
+
+                {captchaUrl && <img src={captchaUrl} alt="captcha"/>}
+                {captchaUrl && createField("Symbols from image", "captcha", [required], Input)}
+
                 {error && <div>{error}</div>}
                 <div>
                     <button>Login</button>
@@ -27,11 +31,11 @@ const LoginForm = ({handleSubmit, error}) => {
 
 const LoginReduxForm = reduxForm({form: "login"})(LoginForm)
 
-const Login = ({login, isAuth}) => {
+const Login = ({login, isAuth, captchaUrl}) => {
     const onSubmit = (formData) => {
         // console.log(formData);
         // props.login comes from connect mapDispatchToProp
-        login(formData.email, formData.password, formData.rememberMe);
+        login(formData.email, formData.password, formData.rememberMe, formData.captcha);
     }
 
     if (isAuth) {
@@ -41,12 +45,13 @@ const Login = ({login, isAuth}) => {
     return (
         <div>
             <h1>Login</h1>
-            <LoginReduxForm onSubmit={onSubmit}/>
+            <LoginReduxForm onSubmit={onSubmit} captchaUrl={captchaUrl}/>
         </div>
     );
 }
 
 const mapStateToProps = (state) => ({
+    captchaUrl: state.auth.captchaUrl,
     isAuth: state.auth.isAuth
 })
 
